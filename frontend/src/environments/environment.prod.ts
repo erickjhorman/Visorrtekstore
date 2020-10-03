4f480d6818e29e32eb30392c7888d24898901c56
Erick <erickjhorman@gmail.com>
2020-10-03 12:52:07 -0500

    Last changes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,4 +3,6 @@ export const environment = {
   name: '',
   stripekey: 'pk_test_l7iYANEOx13w718rnvfY7wed00HkXGcBvC',
   API_URL: 'https://visor-tek.herokuapp.com/api',
+  PUSHER_API_KEY: '3c5c6967326a2c53e2be',
+  PUSHER_CLUSTER: 'us2',
 };
